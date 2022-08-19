e4cb1e9db80f0f05757d044f97c9cc0614e6f9e9
julietadicio <julietadicio@gmail.com>
2022-08-18 23:03:35 -0300

    Agregando parseInt() para sumar, y no concatenar

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
 //function promedio(promedio1, promedio2, promedio3){
 //}
-
-
-
 function promediar() {
-    let promedio1 = prompt('Ingresá nota de 1');
-    let promedio2 = prompt('Ingresá nota de 2');
-    let promedio3 = prompt('Ingresá nota de 3');
-    let resultado = (Number(promedio1) + Number(promedio2) + Number(promedio3))
-    alert('Tu resultado es ' + parseInt(resultado));
+    let promedio1 = prompt('Ingresá tu nota final del primer trimestre');
+    let promedio2 = prompt('Ingresá tu nota final del segundo trimestre');
+    let promedio3 = prompt('Ingresá tu nota final del tercer trimestre');
+    let resultado = (parseInt(promedio1) + parseInt(promedio2) + parseInt(promedio3))
+    alert('La suma de tus notas es ' + resultado + ', ahora vamos a calcular si aprobaste la materia.');
     if (resultado > 21) {
-            alert('Aprobaste!')
+            alert('Aprobaste! Disfruta tus vacaciones, felicidades :)')
         } else {
-            alert('Desaprobaste:(')
+            alert('Desaprobaste, parece que nos vemos en Diciembre :(')
         }
 }
-
-
-/*for (let i = 1; i = 3; i++) {
-    let promedio = prompt('Ingresa la nota del' + i + 'trimestre');
-}
-*/
