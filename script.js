be956fd81e1c114a874e9236737d9b61c06b9841
julietadicio <julietadicio@gmail.com>
2022-08-18 22:21:27 -0300

    Changeing SCSS index.html

---
+++ script.js
@@ -0,0 +1,15 @@
+//function promedio(promedio1, promedio2, promedio3){
+//}
+function promediar() {
+    let promedio1 = prompt('Ingresa la nota del PRIMER trimestre');
+  }
+
+class Persona {
+    constructor(nombre, edad, calle) {
+        this.nombre = nombre; //this.propiedad = valor inicial
+        this.edad = edad; 
+        this.calle = calle;
+    }
+}
+const persona1 = new Persona("Homero", 39, "calle123")
+console.log(persona1)
