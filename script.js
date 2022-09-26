2cf477f43741a17aa48fd2ac4594054756da0a96
julietadicio <julietadicio@gmail.com>
2022-09-26 15:20:28 -0300

    finalizando implementación de Bored Api

--- script.js
+++ script.js
@@ -115,26 +115,25 @@ enviar.addEventListener('click', function (e) {
 })
 
 
-
-// const lista = document.querySelector('#listado')
-
+// const datosApi = document.querySelector('#api')
 // fetch('https://www.boredapi.com/api/activity?type=recreational')
 //     .then((response) => response.json())
 //     .then((data) => {
 //         data.forEach(post => {
 //             const li = document.createElement('li')
-//             li.innerHTML = 
+//             li.innerHTML =
 //             `
 //             <h5>${post.activity}</h5>
 //             <h5>${post.type}</h5>
 //             `
-//            lista.append(li)
+//            datosApi.append(li)
 //         })
 //     })
 
 
-// const lista = document.querySelector('#listado')
 
+
+// const datosApi = document.querySelector('#api')
 // fetch('https://jsonplaceholder.typicode.com/posts')
 //     .then((response) => response.json())
 //     .then((data) => {
@@ -146,9 +145,30 @@ enviar.addEventListener('click', function (e) {
 //             <h5>${post.body}</h5>
 //             `
 
-//             lista.append(li)
+//             datosApi.append(li)
 //         })
 //     })
 
+const url = 'https://www.boredapi.com/api/activity/'
 
+fetch(url)
+.then(response => response.json())
+.then(data => {
 
+    let verApi = document.getElementById('api')
+    let precioApi = document.getElementById('precioApi')
+    verApi.innerHTML =
+    `
+    <h4>${data.activity}</h4>
+    <p>Para hacerlo de a ${data.participants} persona/s.</p>
+    <p>Tipo de actividad: ${data.type} </p>
+    <p>${data.link}</p>
+    `
+    if(data.price > 0) {
+        precioApi.innerHTML = 'Esta actividad puede costar dinero'
+    } else {
+        precioApi.innerHTML = 'Esta actividad es gratuita!'
+    }
+
+    console.log(data);
+})
